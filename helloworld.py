9c19443e1c652a01c547f6024f66153d753346d8
Frank Edward Daza González <frank.daza@cox2m.com>
2020-10-06 19:37:56 -0500

    Escape strings.

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,5 @@
 print('Hello, World!')
+print(1 + 2)
+print(3 * 4)
+print()
+print("The end", "of", "the", "fucking world", 3)
---
+++ strings.py
@@ -0,0 +1,11 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are very easy to use")
+print('We can even include "quotes" in strings')
+print("holi 'caracoli'")
+print("hello" + "world")
+
+greeting = "Hello"
+name = input("Please enter your name: ")
+# if we want a space, we can add that too
+print(greeting + " " + name)
