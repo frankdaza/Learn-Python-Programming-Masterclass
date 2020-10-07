47d3f6e93ce2aa0f7a0002b7cba044f2e762d2e9
Frank Edward Daza González <frank.daza@cox2m.com>
2020-10-06 22:06:37 -0500

    The str String Data Type.

---
+++ operators.py
@@ -0,0 +1,21 @@
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+print()
+
+for i in range(1, 4):
+    print(i)
+
+print()
+
+print(a + b / 3 - 4 * 12)
+print(a + (b / 3) - (4 * 12))
+print(((((a + b) / 3) - 4) * 12))
+print((((a + b) / 3 - 4) * 12))
