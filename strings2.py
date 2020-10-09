dd9693c11eaa2f81114d076e58ba83eee876e538
Frank Edward Daza González <frank.daza@cox2m.com>
2020-10-08 21:15:05 -0500

    Using a Step in a Slice.

--- strings2.py
+++ strings2.py
@@ -26,17 +26,30 @@ parrot = "Norwegian Blue"
 # print(parrot[6 - 14])
 # print(parrot[8 - 14])
 
-print(parrot[0:6])
-print(parrot[3:5])
-print(parrot[0:9])
-print(parrot[:9])
-print(parrot[10:14])
-print(parrot[10:])
+# print(parrot[0:6])
+# print(parrot[3:5])
+# print(parrot[0:9])
+# print(parrot[:9])
+# print(parrot[10:14])
+# print(parrot[10:])
+#
+# print()
+#
+# print(parrot[:6] + parrot[6:])
+# print(parrot[:])
+# print(parrot[-14:8])
+# print(parrot[-4:-2])
+# print(parrot[-4:12])
 
-print()
+print(parrot[0:6:2])
+print(parrot[0:6:3])
 
-print(parrot[:6] + parrot[6:])
-print(parrot[:])
-print(parrot[-14:8])
-print(parrot[-4:-2])
-print(parrot[-4:12])
+number = "3,123;432:654.123"
+separators = number[1::4]
+
+print("Separators:", separators)
+
+values = "".join(char if char not in separators else " " for char in number).split()
+print("Values:", values)
+print("List of numbers:", [int(val) for val in values])
+
