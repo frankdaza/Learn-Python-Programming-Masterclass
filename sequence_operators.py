a214ed739cc62f640079363a0e6d653a57a76d6b
Frank Edward Daza González <frank.daza@cox2m.com>
2020-10-08 21:46:25 -0500

    String Operators.

---
+++ sequence_operators.py
@@ -0,0 +1,16 @@
+string1 = "he's"
+string2 = "probably"
+string3 = "pining"
+string4 = "for the"
+string5 = "jfords"
+
+print(string1, string2, string3, string4, string5)
+print("he's " "probably " "pining " "for the " "jfords ")
+
+print("Hello World! " * 10)
+
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thur" in today)
+print("parrot" in "fjord")
