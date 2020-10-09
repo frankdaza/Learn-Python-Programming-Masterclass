edb182a5661c8bcdc4f24c53dba86478259e7938
Frank Edward Daza González <frank.daza@cox2m.com>
2020-10-08 21:27:03 -0500

    Slicing Backwards.

---
+++ sliceback.py
@@ -0,0 +1,12 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+backwards = letters[::-1]
+print("Backwards:", backwards)
+
+qpo = letters[16:13:-1]
+print("QPO:", qpo)
+
+edcba = backwards[21:]
+print("EDCBA:", edcba)
+
+hgfedcba = backwards[18:]
+print("HGFEDCBA:", hgfedcba)
