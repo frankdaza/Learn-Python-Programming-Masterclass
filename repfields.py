d5c7f2dfdfd538efccdb6267777d3b66c97b77aa
Frank Edward Daza González <frank.daza@cox2m.com>
2020-10-08 22:03:42 -0500

    String Replacement Fields.

---
+++ repfields.py
@@ -0,0 +1,6 @@
+age = 31
+print("My age is " + str(age) + " years old.")
+print("My age is {0} years old.".format(age))
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
+      .format(31, "Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"))
