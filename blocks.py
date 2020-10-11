61a715f7b3c51e29366c84a4c268dc2de2508c17
Frank Edward Daza González <frank.daza@cox2m.com>
2020-10-11 18:16:44 -0500

    elif.

--- blocks.py
+++ blocks.py
@@ -7,14 +7,16 @@ name = input("Please enter your name: ")
 age = int(input(f"How old are you {name}? "))
 print(age)
 
-if age >= 18:
-    print("You're old enough to vote!")
-    print("Please put an X in the box")
-else:
-    print(f"Please come back in {18 - age} years")
+# if age >= 18:
+#     print("You're old enough to vote!")
+#     print("Please put an X in the box")
+# else:
+#     print(f"Please come back in {18 - age} years")
 
 if age < 18:
     print(f"Please come back in {18 - age} years")
+elif age == 900:
+    print("Sorry Yoda, you died in: Return of the Jedi!")
 else:
     print("You're old enough to vote!")
     print("Please put an X in the box")
