c320d93bc5b683895080fafd11779e3e06d267c0
Frank Edward Daza González <frank.daza@cox2m.com>
2020-10-26 13:40:43 -0500

    Binding Multiple Names to a List.

--- sequences/mutable.py
+++ sequences/mutable.py
@@ -6,3 +6,11 @@ print(id(another_list))
 shopping_list += ["cookies"]
 print(shopping_list)
 print(id(shopping_list))
+
+a = b = c = d = e = f = another_list
+print(a)
+
+print("Adding cream")
+b.append("cream")
+print(c)
+print(d)
