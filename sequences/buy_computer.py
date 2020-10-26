2dcf2d3e392f68045e7e8a8d15360bbeba226c1f
Frank Edward Daza González <frank.daza@cox2m.com>
2020-10-26 14:22:27 -0500

    Improving our Code.

--- sequences/buy_computer.py
+++ sequences/buy_computer.py
@@ -1,26 +1,19 @@
-available_parts = ["computer", "monitor", "keyboard", "mouse", "mouse mat", "hdmi calbe"]
+available_parts = ["computer", "monitor", "keyboard", "mouse", "mouse mat", "hdmi calbe", "dvd driver"]
 current_choice = "-"
 computer_parts = []
 
+valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
+
 while current_choice != '0':
-    if current_choice in "123456":
+    if current_choice in valid_choices:
         print(f"Adding {current_choice}")
-        if current_choice == '1':
-            computer_parts.append("computer")
-        elif current_choice == '2':
-            computer_parts.append("monitor")
-        elif current_choice == '3':
-            computer_parts.append("keyboard")
-        elif current_choice == '4':
-            computer_parts.append("mouse")
-        elif current_choice == '5':
-            computer_parts.append("mouse mat")
-        elif current_choice == '6':
-            computer_parts.append("hdmi cable")
+        index = int(current_choice)
+        chosen_part = available_parts[index - 1]
+        computer_parts.append(chosen_part)
     else:
         print("Please add options from the list below:")
-        for part in available_parts:
-            print(f"{available_parts.index(part) + 1}: {part}")
+        for number, part in enumerate(available_parts):
+            print(f"{number + 1}: {part}")
 
     current_choice = input()
 
